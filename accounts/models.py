2bcd683c02257f8da0a8e75d82cb38b7eddf67e5
Kaustubh Dixit <kdkishu335@gmail.com>
2019-07-15 02:54:36 +0530

    Updated admin.

--- accounts/models.py
+++ accounts/models.py
@@ -4,18 +4,19 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 
-
 # Create your models here.
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     voter_id = models.CharField(max_length=20, blank=False)
     auth_id = models.CharField(max_length=20, blank=False)
 
-    @receiver(post_save, sender=User)
-    def create_user_profile(sender, instance, created, **kwargs):
-        if created:
-            Profile.objects.create(user=instance)
 
-    @receiver(post_save, sender=User)
-    def save_user_profile(sender, instance, **kwargs):
-        instance.profile.save()
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
