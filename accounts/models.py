d6f57afb759317c8071b95ee3742b2262035376f
Kaustubh Dixit <kdkishu335@gmail.com>
2019-07-15 02:46:52 +0530

    Updated User model.

--- accounts/models.py
+++ accounts/models.py
@@ -1,8 +1,21 @@
 from django.db import models
-from django.contrib import auth
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+
 
 # Create your models here.
-class User(auth.models.User, auth.models.PermissionsMixin):
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    voter_id = models.CharField(max_length=20, blank=False)
+    auth_id = models.CharField(max_length=20, blank=False)
 
-    def __str__(self):
-        return "@{}".format(self.username)
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()
--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,13 @@
 from django.views.generic import TemplateView
 from django.contrib.auth.views import LoginView as DefaultLoginView
 from codefundo.views import HomepageView
+from .models import Profile, User
 
 
 class LoginView(DefaultLoginView, HomepageView):
     template_name = 'accounts/login.html'
     redirect_authenticated_user = True
+
+def update_profile(request, user_id):
+    user = User.objects.get(pk=user_id)
+    user.save()
