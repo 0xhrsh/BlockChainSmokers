5dd02918877de14bdc154e470576f7059c9a7894
Kaustubh Dixit <kdkishu335@gmail.com>
2019-07-20 00:04:07 +0530

    Added method decorator.

--- codefundo/views.py
+++ codefundo/views.py
@@ -1,15 +1,24 @@
 from django.views.generic import TemplateView
 from django.http import HttpResponse
+from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import login_required
+
 
 class HomepageView(TemplateView):
     template_name = 'index.html'
 
+
 class TestPageView(TemplateView):
     template_name = 'profile.html'
+
+    @method_decorator(login_required)
+    def dispatch(self, *args, **kwargs):
+        return super().dispatch(*args, **kwargs)
 
 
 class ThanksView(TemplateView):
     template_name = 'thanks.html'
+
 
 def detail(request, candidate_id):
     return HttpResponse("You're looking at candidate %s." % candidate_id)
