943028c6cc10d30d4b2042b28702291d512a296c
Kaustubh Dixit <kdkishu335@gmail.com>
2019-07-20 23:56:48 +0530

    Enables adding and displaying the candidate through the django admin.

--- codefundo/views.py
+++ codefundo/views.py
@@ -2,14 +2,23 @@ from django.views.generic import TemplateView
 from django.http import HttpResponse
 from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required
+from accounts.models import Candidate
 
 
-class TestPageView(TemplateView):
+class ProfilePageView(TemplateView):
     template_name = 'codefundo/profile.html'
 
     @method_decorator(login_required)
     def dispatch(self, *args, **kwargs):
         return super().dispatch(*args, **kwargs)
+
+class VoteHelperPageView(TemplateView):
+    template_name = 'codefundo/helper.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(VoteHelperPageView, self).get_context_data()
+        context['candidates'] = Candidate.objects.all()
+        return context
 
 
 class ThanksView(TemplateView):
